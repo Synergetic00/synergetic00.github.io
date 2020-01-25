3389c30dfa431e50eaae8d4785e1a9c5b754b4ae
Synergetic00 <synergetic00@gmail.com>
2020-01-25 13:26:55 +1100

    New Version

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const navSlide = () => {
     const menu = document.querySelector('.menu');
-    const nav = document.querySelector('.nav_links');
-    const navs = document.querySelectorAll('.nav_links');
+    const nav = document.querySelector('.nav-links');
+    const navs = document.querySelectorAll('.nav-links');
 
     menu.addEventListener('click', () => {
         nav.classList.toggle('nav_active');
-        
+        console.log("Hi");
     });
 
     navs.forEach((link, index) => {
